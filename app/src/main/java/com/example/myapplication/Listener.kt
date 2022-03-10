b81678dbf1ac92ebf7b7cd72cd42612f3e69e43e
kimghem123 <82371258+kimghem123@users.noreply.github.com>
2022-03-10 23:57:51 +0900

    update

---
+++ app/src/main/java/com/example/myapplication/Listener.kt
@@ -0,0 +1,24 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_listener.*
+
+class Listener : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listener)
+        //뷰를 activity로 가져오는 방법
+        //1> 직접 찾아서 가져온다
+//        val textView : TextView = findViewById(R.id.hello)
+        //2> xml을 import해서 가져온다
+//        hello.
+
+        hello.setOnClickListener {
+            Log.d("Click","Click")
+        }
+
+    }
+}
