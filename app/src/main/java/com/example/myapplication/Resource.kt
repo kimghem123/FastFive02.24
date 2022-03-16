009a0860746044dc937a36d9ef66803377196126
kimghem123 <82371258+kimghem123@users.noreply.github.com>
2022-03-16 15:58:34 +0900

    update

---
+++ app/src/main/java/com/example/myapplication/Resource.kt
@@ -0,0 +1,29 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_resource.*
+
+class Resource : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resource)
+
+        //1
+        val ment = resources.getString(R.string.hello)
+        Log.d("mentt",ment)
+
+        //2
+        val ment2 = getString(R.string.hello)
+        Log.d("mentt",ment2)
+
+        val color = getColor(R.color.textview_color)
+
+        button.setOnClickListener{
+            Log.d("color","what")
+            button.setBackgroundColor(color)
+         }
+    }
+
+}
