c319d5482c5e675c23453d0a2307a2e963a6b129
kimghem123 <82371258+kimghem123@users.noreply.github.com>
2022-05-16 21:54:11 +0900

    update

--- app/src/main/java/com/example/myapplication/TapPagerActivity.kt
+++ app/src/main/java/com/example/myapplication/TapPagerActivity.kt
@@ -2,10 +2,45 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+import kotlinx.android.synthetic.main.activity_tap_pager.*
 
 class TapPagerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tap_pager)
+
+        tab_layout.addTab(tab_layout.newTab().setText("ONE"))
+        tab_layout.addTab(tab_layout.newTab().setText("TWO"))
+        tab_layout.addTab(tab_layout.newTab().setText("THREE"))
     }
 }
+
+class PagerAdapter(
+    fragmentManager: FragmentManager,
+    val tabCount: Int
+): FragmentStatePagerAdapter(fragmentManager){
+    override fun getCount(): Int {
+        return tabCount
+    }
+
+    override fun getItem(position: Int): Fragment {
+        when(position){
+            0 -> {
+                return fragment1()
+            }
+            1 -> {
+                return fragment2()
+            }
+            2 -> {
+                return fragment3()
+            }
+            else -> {
+                return fragment1()
+            }
+        }
+    }
+}
+
