311b2c66f5d9e515ec1cf39160a44a29aafe4c7f
kimghem123 <82371258+kimghem123@users.noreply.github.com>
2022-05-24 02:02:50 +0900

    update

--- app/src/main/java/com/example/myapplication/TapPagerActivity.kt
+++ app/src/main/java/com/example/myapplication/TapPagerActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
+import com.google.android.material.tabs.TabLayout
 import kotlinx.android.synthetic.main.activity_tap_pager.*
 
 class TapPagerActivity : AppCompatActivity() {
@@ -15,10 +16,29 @@ class TapPagerActivity : AppCompatActivity() {
         tab_layout.addTab(tab_layout.newTab().setText("ONE"))
         tab_layout.addTab(tab_layout.newTab().setText("TWO"))
         tab_layout.addTab(tab_layout.newTab().setText("THREE"))
+
+
+        val pagerAdapter = FragmentPagerAdapter(supportFragmentManager,3)
+        view_pager.adapter = pagerAdapter
+
+        tab_layout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                view_pager.currentItem = tab!!.position
+                //탭이 클릭되었을떄
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
+        view_pager.addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+        // 슬라이드로 탭이 바뀌면 바뀌었다는걸 알려줌줌
     }
 }
 
-class PagerAdapter(
+class FragmentPagerAdapter(
     fragmentManager: FragmentManager,
     val tabCount: Int
 ): FragmentStatePagerAdapter(fragmentManager){
--- app/src/main/java/com/example/myapplication/fragment3.kt
+++ app/src/main/java/com/example/myapplication/fragment3.kt
@@ -17,6 +17,6 @@ class fragment3:Fragment(){
         //프라그먼트가 인터페이스를 처음으로 그릴 때 호출된다.
         //inflater -> 뷰를 그려주는 역할
         //container -> 부모 뷰
-        return inflater.inflate(R.layout.fragment_one,container,false)
+        return inflater.inflate(R.layout.fragment_three,container,false)
     }
 }
